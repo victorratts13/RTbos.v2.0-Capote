d878722ac5f9a2cf45fed6237362240b35e68a90
Victor Ratts <victor.ratts13@gmail.com>
2019-06-20 08:13:55 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,36 +34,44 @@ const lucro = api.lucro; //porcentagem de lucro, caso esteja vazio o lucro esper
 
 setInterval(() => {
     console.log('\033c Bem vindo ao RTbos v2.0 (capote) '+ cont++)
-    clearModule('./var/tempFile')
-    const tempFile = require('./var/tempFile')
-    if(api.invest == false){
-        var link = api.local;
-        request({
-            url: link+`/history?symbol=${par}&resolution=300&from=${start}&to=${end}`,
-            json: true
-        }, (err, response, body) => {
-            var renkoBrick = body;
-            if(err){
-                console.log(err)
-            }else{
-                poloniex.returnCurrencies((err, currencies) => {
-                    if(err){
-                        console.log('Currencie -> '+err)
-                    }else{
-                        poloniex.returnBalances((err, balance) => {
-                            if(err){
-                                console.log('Balances -> '+err)
-                            }else{
-                                ma1 = SMA.calculate({period: api.ma1, values: renkoBrick.c})
-                                ma2 = SMA.calculate({period: api.ma2, values: renkoBrick.c})
-                                //console.log(ma1)
-                                //console.log(renkoBrick.c)
-                                var Ma01_val = ma1.slice(-1)[0];
-                                var Ma02_val = ma2.slice(-1)[0];
-                                console.log('BTC saldo -> '+balance.BTC )
-                                console.log('USDT saldo -> '+balance.USDT )
-                                console.log('Media Movel: '+api.ma1+' -> '+Ma01_val);
-                                console.log('Media Movel: '+api.ma2+' -> '+Ma02_val);
+    poloniex.returnTicker((err, ticker) => {
+        var lowesk = ticker.USDT_BTC.lowestAsk;
+        var highest = ticker.USDT_BTC.highestBid;
+
+           
+        clearModule('./var/tempFile')
+        const tempFile = require('./var/tempFile')
+        if(api.invest == false){
+            var link = api.local;
+            request({
+                url: link+`/history?symbol=${par}&resolution=300&from=${start}&to=${end}`,
+                json: true
+            }, (err, response, body) => {
+                var renkoBrick = body;
+                if(err){
+                    console.log(err)
+                }else{
+                    poloniex.returnCurrencies((err, currencies) => {
+                        if(err){
+                            console.log('Currencie -> '+err)
+                        }else{
+                            poloniex.returnBalances((err, balance) => {
+                                if(err){
+                                    console.log('Balances -> '+err)
+                                }else{
+                                    ma1 = SMA.calculate({period: api.ma1, values: renkoBrick.c})
+                                    ma2 = SMA.calculate({period: api.ma2, values: renkoBrick.c})
+                                    //console.log(ma1)
+                                    //console.log(renkoBrick.c)
+
+                                    var Ma01_val = ma1.slice(-2)[0];
+                                    var Ma02_val = ma2.slice(-2)[0];
+                                    console.log('lowesk -> '+lowesk);
+                                    console.log('highest -> '+highest);
+                                    console.log('BTC saldo -> '+balance.BTC )
+                                    console.log('USDT saldo -> '+balance.USDT )
+                                    console.log('Media Movel: '+api.ma1+' -> '+Ma01_val);
+                                    console.log('Media Movel: '+api.ma2+' -> '+Ma02_val);
 //====================================== cruzamento de medias em renko ============================================
                                 function cross(){//cross function 
                                     if(Ma01_val > Ma02_val){
@@ -75,68 +83,79 @@ setInterval(() => {
                                     return 0;
                                 }
 //======================================= base de referencia ========================================================
-                                var price = Ma01_val;
-                                var buyVal = (price * balance.USDT / price);
-                                var sellVal = (price * balance.BTC / price);
-                                var USDT_buy = parseInt(buyVal);
-                                console.log('Preço -> '+price);
-                                console.log('valor de compra -> '+buyVal);
-                                console.log('valor de venda -> '+sellVal);
-                                console.log('USDT valor int ->'+USDT_buy)
+                            poloniex.returnChartData(par, period, start, end, (err, data) => {
+                                data = data.slice(-1)
+                                console.log(data[0].close)
+
+                                    //var price = renkoBrick.c.slice(-1);
+                                    //var rate = parseInt(renkoBrick.c.slice(-1))
+                                    var price = data[0].close;
+                                    var rate = parseInt(data[0].close);
+                                    var buyVal = (balance.USDT / lowesk);
+                                    var sellVal = (price * balance.BTC / price);
+                                    var USDT_buy = parseInt(buyVal);
+                                    console.log('Preço -> '+price);
+                                    console.log('rate -> '+rate)
+                                    console.log('valor de compra -> '+buyVal);
+                                    console.log('valor de venda -> '+sellVal);
+                                    console.log('USDT valor int ->'+USDT_buy)
 //======================================= funções de compra / venda =================================================
-                                function buy(){
-                                    poloniex.buy(par, price, buyVal, 1, 0, 0, (err, response) => {
-                                        if(err){
-                                            console.log('algum erro ocorreu -> '+err)
-                                        }else{
-                                            console.log(JSON.stringify(response))        
-                                        }
-                                    })
-                                }
+                                    function buy(){
+                                        poloniex.buy(par, rate, buyVal, 1, 0, 0, (err, response) => {
+                                            if(err){
+                                                console.log('algum erro ocorreu na compra -> '+err)
+                                            }else{
+                                                console.log(JSON.stringify(response))        
+                                            }
+                                        })
+                                    }
 
-                                function sell(){
-                                    poloniex.sell(par, price, sellVal, 1, 0, 0, (err, response) => {
-                                        if(err){
-                                            console.log('algum erro ocorreu -> '+err)
-                                        }else{
-                                            console.log(response)
-                                        }
-                                    })
-                                }
+                                    function sell(){
+                                        poloniex.sell(par, rate, sellVal, 1, 0, 0, (err, response) => {
+                                            if(err){
+                                                console.log('algum erro ocorreu na venda -> '+err)
+                                            }else{
+                                                console.log(response)
+                                            }
+                                        })
+                                    }
+
+                                    
 //======================================= execução das ordens ========================================================
-                                if(cross() == 1){
-                                    if(tempFile.type == 'sell'){
-                                        console.log('executando compra');
-                                        buy();
-                                        var createTemp = "var lastOrder = {type: 'buy'}; module.exports = lastOrder;"
-                                        fs.writeFile('./var/tempFile.js', createTemp, (err) => {
-                                            if(err){
-                                                console.log(err)
-                                            }else{
-                                                console.log('criado arquivo temporario -> Buy')
-                                            }
-                                        })
-                                    }else{
-                                        console.log('compra executada, aguardando venda')
-                                    }
+                                    if(cross() == 1){
+                                        if(tempFile.type == 'sell'){
+                                            console.log('executando compra');
+                                            buy();
+                                            var createTemp = "var lastOrder = {type: 'buy'}; module.exports = lastOrder;"
+                                            fs.writeFile('./var/tempFile.js', createTemp, (err) => {
+                                                if(err){
+                                                    console.log(err)
+                                                }else{
+                                                    console.log('criado arquivo temporario -> Buy')
+                                                }
+                                            })
+                                        }else{
+                                            console.log('compra executada, aguardando venda')
+                                        }
 
-                                }
-                                if(cross() == 2){
-                                    if(tempFile == 'buy'){
-                                        var createTemp = "var lastOrder = {type: 'sell'}; module.exports = lastOrder;"
-                                        console.log('executando venda');
-                                        sell()
-                                        fs.writeFile('./var/tempFile.js', createTemp, (err) => {
-                                            if(err){
-                                                console.log(err)
-                                            }else{
-                                                console.log('criado arquivo temporario -> Sell')
-                                            }
-                                        })
-                                    }else{
-                                        console.log('venda executada, aguardando compra')                                        
                                     }
-                                }                                                                                               
+                                    if(cross() == 2){
+                                        if(tempFile == 'buy'){
+                                            var createTemp = "var lastOrder = {type: 'sell'}; module.exports = lastOrder;"
+                                            console.log('executando venda');
+                                            sell()
+                                            fs.writeFile('./var/tempFile.js', createTemp, (err) => {
+                                                if(err){
+                                                    console.log(err)
+                                                }else{
+                                                    console.log('criado arquivo temporario -> Sell')
+                                                }
+                                            })
+                                        }else{
+                                            console.log('venda executada, aguardando compra')                                        
+                                        }
+                                    }
+                                })                                                                                                   
                             }
                         })
                     }
@@ -144,6 +163,7 @@ setInterval(() => {
             }
         })
     }
+}) 
 //##################################################################################################################
 //################################################### Fim do Codigo ################################################
 //##################################################################################################################
